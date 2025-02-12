69323078579753ddc7ef47c2704561636b683db8
Hugo <hugoribeirorneto@gmail.com>
2025-02-11 22:05:00 -0300

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import classes.User;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
 
-        System.out.println("Hello world!");
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");
+        User usuario = new User();
+
+        System.out.print("Por favor, digite seu nome: ");
+        usuario.setNome(sc.nextLine());
+        System.out.println("Bem-vindo, " + usuario.getNome() + ". Por favor, escolha uma opção: ");
+        System.out.println("1. Iniciar atendimento");
+        System.out.println("2. Finalizar o sistema");
+
+
+
     }
 }
---
+++ src/classes/User.java
@@ -0,0 +1,21 @@
+package classes;
+
+public class User {
+    private String nome;
+
+    public User(String nome) {
+        this.nome = nome;
+    }
+
+    public User() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
